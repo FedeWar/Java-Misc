e032f325ef66be6d0d992c36c2ac6d1c56bac297
FedeWar <federico.guerra.96@gmail.com>
2016-07-20 22:50:47 +0200

    Aggiungendo OpenGL

--- NBodySimulation/src/it/FedeWar/NBody2D/Applicazione.java
+++ NBodySimulation/src/it/FedeWar/NBody2D/Applicazione.java
@@ -2,6 +2,7 @@ package it.FedeWar.NBody2D;
 
 import it.FedeWar.NBody2D.Engine.Engine;
 import it.FedeWar.NBody2D.Engine.Sim_Info;
+import it.FedeWar.NBody2D.GUI.GLWin;
 import it.FedeWar.NBody2D.GUI.MainWin;
 import it.FedeWar.NBody2D.GUI.SimulationWin;
 
@@ -16,7 +17,8 @@ public class Applicazione
 
 		if(SI.opengl)
 		{
-
+			GLWin GW = new GLWin();
+			GW.open();
 		}
 		else
 		{
