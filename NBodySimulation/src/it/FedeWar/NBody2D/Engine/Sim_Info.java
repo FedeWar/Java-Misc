86ac175fbbe3cfcd3fdf6c69f7facb5b02b929fe
FedeWar <federico.guerra.96@gmail.com>
2016-07-29 13:38:40 +0200

    Aggiunte due costanti

--- NBodySimulation/src/it/FedeWar/NBody2D/Engine/Sim_Info.java
+++ NBodySimulation/src/it/FedeWar/NBody2D/Engine/Sim_Info.java
@@ -6,6 +6,8 @@ import java.awt.Dimension;
  * La quantità delle informazioni cambia a seconda del tipo
  * di simulazione, questa classe fornisce una base per
  * implementare ogni tipo di simulazione.
+ * Alcune proprietà sono già implementate, sono quelle
+ * base che ogni simulazione dovrebbe usare.
  * 
  * @author FedeWar
  * @version 1.0
@@ -14,6 +16,33 @@ import java.awt.Dimension;
  * */
 public abstract class Sim_Info
 {
+	/**
+	 * La quantità di oggetti da creare e utilizzare.
+	 * 
+	 * @since 1.0
+	 */
 	public int obj_count;
+	
+	/**
+	 * La costante di gravitazione con cui calcolare
+	 * l'accelerazione.
+	 * 
+	 * @since 1.0
+	 */
+	public double G;
+	
+	/**
+	 * Il passo temporale tra due frame.
+	 * 
+	 * @since 1.0
+	 */
+	public double deltaT;
+	
+	/**
+	 * Le dimensioni della finestra su cui visualizzare
+	 * la simulazione.
+	 * 
+	 * @since 1.0
+	 */
 	public Dimension winDim;
 }
