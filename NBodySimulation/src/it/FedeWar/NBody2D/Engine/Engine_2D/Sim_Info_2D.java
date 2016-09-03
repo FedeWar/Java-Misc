c8eb0dfb3e320a4f24c2577f48af136b8c08c7cd
FedeWar <federico.guerra.96@gmail.com>
2016-09-03 11:47:44 +0200

    Modifiche ai commenti.
    Sim_Info non determina più una specifica, la classe non ha bisogno di
    essere ampliata quindi è stata rimossa la documentazione.

--- NBodySimulation/src/it/FedeWar/NBody2D/Engine/Engine_2D/Sim_Info_2D.java
+++ NBodySimulation/src/it/FedeWar/NBody2D/Engine/Engine_2D/Sim_Info_2D.java
@@ -2,55 +2,31 @@ package it.FedeWar.NBody2D.Engine.Engine_2D;
 
 import java.awt.Dimension;
 
-/**
- * La quantità delle informazioni cambia a seconda del tipo
- * di simulazione, questa classe fornisce una base per
- * implementare ogni tipo di simulazione.
- * Alcune proprietà sono già implementate, sono quelle
- * base che ogni simulazione dovrebbe usare.
- * 
- * @author FedeWar
- * @version 1.0
- * @since 1.0
- * @date 24/07/2016
- * */
+import com.sun.javafx.geom.Vec3d;
+
+/* Collezione di informazioni sulla simulazione 2D. */
 public class Sim_Info_2D
 {
-	/**
-	 * La quantità di oggetti da creare e utilizzare.
-	 * 
-	 * @since 1.0
-	 */
+	/* La quantità di oggetti da creare e utilizzare. */
 	public int obj_count;
 	
-	/**
-	 * La costante di gravitazione con cui calcolare
-	 * l'accelerazione.
-	 * 
-	 * @since 1.0
-	 */
+	/* La costante di gravitazione con cui calcolare l'accelerazione. */
 	public double G;
 	
-	/**
-	 * Il passo temporale tra due frame.
-	 * 
-	 * @since 1.0
-	 */
+	/* Il passo temporale tra due frame. */
 	public double deltaT;
 	
-	/**
-	 * Le dimensioni della finestra su cui visualizzare
-	 * la simulazione.
-	 * 
-	 * @since 1.0
-	 */
+	/* Le dimensioni della finestra su cui visualizzare la simulazione. */
 	public Dimension winDim;
 	
+	/* Informazioni sulla massa degli oggetti. */
 	public int standard_mass;
 	public int mass_variation;
 	
+	/* Informazioni sul raggio degli oggetti. */
 	public int standard_radius;
 	public int radius_variation;
 	
-	public double[] spaceDim;
+	/* Dimensioni dello spazio in cui possono spawnare oggetti. */
+	public Vec3d spaceDim;
 }
