9f2af8124c55213319334e04d097ebd72abdf2f0
FedeWar <federico.guerra.96@gmail.com>
2016-10-22 22:17:20 +0200

    Rimosso winDim

--- NBodySimulation/src/it/FedeWar/NBody/Engine/Engine_2D/Sim_Info_2D.java
+++ NBodySimulation/src/it/FedeWar/NBody/Engine/Engine_2D/Sim_Info_2D.java
@@ -1,7 +1,5 @@
 package it.FedeWar.NBody.Engine.Engine_2D;
 
-import java.awt.Dimension;
-
 import com.sun.javafx.geom.Vec2d;
 
 /* Collezione di informazioni sulla simulazione 2D. */
@@ -16,9 +14,6 @@ public class Sim_Info_2D
 	/* Il passo temporale tra due frame. */
 	public double deltaT;
 	
-	/* Le dimensioni della finestra su cui visualizzare la simulazione. */
-	public Dimension winDim;
-	
 	/* Informazioni sulla massa degli oggetti. */
 	public int standard_mass;
 	public int mass_variation;
@@ -29,4 +24,7 @@ public class Sim_Info_2D
 	
 	/* Dimensioni dello spazio in cui possono spawnare oggetti. */
 	public Vec2d spaceDim;
+
+	/* Percorso verso cui esportare il video. */
+	public String export;
 }
